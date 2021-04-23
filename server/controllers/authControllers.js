21e390d1320ca11ad336fb6ad203d7d1ec6b9c18
Dvaid Adeleye <drizzzydave@gmail.com>
2021-04-23 23:18:19 +0100

    initial folder structure setup, creating controllers

---
+++ server/controllers/authControllers.js
@@ -0,0 +1,10 @@
+const login = (req, res, next) => {};
+
+const register = (req, res, next) => {};
+const forgotPassword = (req, res, next) => {};
+const resetPassword = (req, res, next) => {};
+
+exports.login = login;
+exports.register = register;
+exports.forgotPassword = forgotPassword;
+exports.resetPassword = resetPassword;
