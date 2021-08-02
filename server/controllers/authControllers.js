a81a552cb5ba23196619f9f0312e8ad72cff7c7e
Dvaid Adeleye <drizzzydave@gmail.com>
2021-08-02 16:59:52 +0100

    setting up jwt

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,20 +1,21 @@
 const User = require("../models/User");
+const HttpError = require("../utils/httpError");
 
 const login = async (req, res, next) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    res.status(400).json({ message: " Please provide email and password" });
+    next(new HttpError("Please provide email and password", 400));
   }
   try {
     const user = await User.findOne({ email }).select("+password");
     if (!user) {
-      res.status(404).json({ message: "Invalid credentials" });
+      next(new HttpError("Invalid credentials", 404));
     }
 
     const isMatch = await user.matchPasswords(password);
 
     if (!isMatch) {
-      res.status(404).json({ message: "Invalid Credentials" });
+      next(new HttpError("Invalid Credentials", 404));
     }
 
     res.status(200).json({ token: "ogibcibkb" });
