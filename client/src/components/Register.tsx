64698823d678b2bc72ea566fe46bc3bbbc425cb6
Dvaid Adeleye <drizzzydave@gmail.com>
2021-08-09 13:37:41 +0100

    built register form , leveraging Bootstrap

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,12 +1,42 @@
-
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import { useForm } from 'react-hook-form'
 const Register = () => {
-    return (
-        <div>
-            <h1 className="">
-                
+    const {register, handleSubmit, formState: { errors } } = useForm()
 
-            </h1>
-        </div>
+    const onSubmit = (data: any) => console.log(data);
+
+    return (
+        <Container> 
+            <Row className="border justify-content-center align-items-center mt-5 h-100"> 
+                 <Col xs={4} className="border"> 
+                    <Form onSubmit={handleSubmit(onSubmit)}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" {...register("email", {required: true})}  isInvalid={!!errors.email} />
+                            <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+              {/*           <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group> */}
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                </Form>
+            </Col>
+            </Row>
+           
+        </Container>
     )
 }
 
