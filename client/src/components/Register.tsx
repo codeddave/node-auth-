4382e5cf9f31b7ff93f87d9b913de7cb45e7d74c
Dvaid Adeleye <drizzzydave@gmail.com>
2021-08-09 15:03:24 +0100

    created Register Schema, used React hook form to validate form

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -3,9 +3,26 @@ import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup'
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form'
+import * as Yup from "yup"   
+ const RegisterSchema = Yup.object().shape({
+        email: Yup.string()
+        .trim()
+        .min(2, "Too Short!")
+        .max(50, "Too Long!")
+        .email("Invalid email")
+        .required("Required"),
+      password: Yup.string()
+        .min(8, "Too Short!")
+        .max(50, "Too Long!")
+        .required("Required"),
+    })
 const Register = () => {
-    const {register, handleSubmit, formState: { errors } } = useForm()
+    const {register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(RegisterSchema)
+    })
 
     const onSubmit = (data: any) => console.log(data);
 
@@ -13,18 +30,25 @@ const Register = () => {
         <Container> 
             <Row className="border justify-content-center align-items-center mt-5 h-100"> 
                  <Col xs={4} className="border"> 
-                    <Form onSubmit={handleSubmit(onSubmit)}>
+                    <Form noValidate onSubmit={handleSubmit(onSubmit)}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" {...register("email", {required: true})}  isInvalid={!!errors.email} />
-                            <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                            </Form.Text>
+                            <InputGroup hasValidation>
+                            
+                            <Form.Control type="email"   placeholder="Enter email" {...register("email", {required: true})}  isInvalid={!!errors.email} />
+                             <Form.Control.Feedback type="invalid">
+                                {errors.email ? errors.email.message: null}
+                            </Form.Control.Feedback>
+                            </InputGroup>
+                           
+                            {/*    <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text> */}
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control type="password" placeholder="Password"  {...register("password", {required: true})}  isInvalid={!!errors.password} />
                         </Form.Group>
               {/*           <Form.Group className="mb-3" controlId="formBasicCheckbox">
                             <Form.Check type="checkbox" label="Check me out" />
