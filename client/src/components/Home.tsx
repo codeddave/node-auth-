bd071b0c5cade2ec7b300798e8f620a6ee7fb874
Dvaid Adeleye <drizzzydave@gmail.com>
2021-08-15 05:03:42 +0100

    implemented logout

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,9 +1,26 @@
 //import React from 'react'
+import Button from 'react-bootstrap/Button'
+import { useHistory } from 'react-router-dom'
+import { toast } from 'react-toastify';
 
 const Home = () => {
+   const history =  useHistory()
+
+    const handleLogout = ()=> {
+        localStorage.removeItem("user")
+        toast.success("Come back soon!")
+        history.push("/login")
+    }
     return (
         <div>
-            <h1>You're home now</h1>
+            <h1 className="text-center mt-5">You're home now</h1>
+            <div className="d-flex justify-content-center">
+                <Button variant="primary" onClick ={handleLogout} className="mt-3 ">
+                    Log Out
+                </Button>
+
+            </div>
+            
         </div>
     )
 }
